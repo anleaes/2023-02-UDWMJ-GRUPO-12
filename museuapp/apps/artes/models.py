3b05adda6fc916ce55eb4942bd3bde27f52a8ba8
GabrielYanzerCarlesso <GabrielYanzerCarlesso@gmail.com>
2023-12-02 21:59:37 -0300

    ajustando listagens

--- museuapp/apps/artes/models.py
+++ museuapp/apps/artes/models.py
@@ -5,7 +5,9 @@ class Arte(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
     name = models.CharField('Nome', max_length=50)
+    url = models.CharField('URL', max_length=200)
     description = models.TextField('Descricao', max_length=100) 
+
     
     class Meta:
         verbose_name = 'Arte'
